15da27e3069a8757ec18420a2a897add4d276721
Shaan <shaban2000m@gmail.com>
2020-07-03 12:35:35 +0400

    back

--- s-main-scripts/js/noti_loginpage.js
+++
@@ -1,6 +0,0 @@
-const notyf = new Notyf({
-    dismissible:true,
-    duration:2000,
-});
-
-notyf.success('Shaban Aliyev InValer.com!');
--- test.php
+++
@@ -1,5 +0,0 @@
-<input type="text" id="test">
-<script>
-        document.getElementById('show').innerHTML = document.getElementById('test').value.toLowerCase();
-</script>
-<p id="show"></p>
